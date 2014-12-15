73986f6056aad4a31e89c62b7257c9ed8ee2484d
Mark Turiansky <mark.turiansky@yahoo.com>
2014-12-14 20:47:18 -0500

    minor changes

--- models.py
+++ models.py
@@ -4,6 +4,15 @@ from datetime import datetime
 from random import random
 import os
 
+class Backends(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	url = db.Column(db.String(160), unique=True)
+	num_files = db.Column(db.Integer)
+
+	def __init__(self,url):
+		self.url = url
+		self.num_files = 0
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80), unique=True)
